f0ec3be0e4d4d11b362ad9e1cbbfa20d3a8233ef
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-05-08 23:57:03 +0200

    12.done

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,8 +1,20 @@
+import {useRef} from "react"
+
 export default function CheckoutForm() {
+    const formEl = useRef(null);
+    function submitted(e) {
+        e.preventDefault();
+        console.log(formEl.current.elements.fullName.value);
+        console.log(formEl.current.elements.address.value);
+    }
   return (
     <div>
-      <form>
-        <input />
+      <form ref={formEl} onSubmit={submitted}>
+        <label htmlFor="fullName">Full name</label>
+        <input required type="text" name="fullName" id="fullName"/>
+        <label htmlFor="address">Address</label>
+        <input required type="text" name="address" id="address"/>
+        <button>Pay</button>
       </form>
     </div>
   );
