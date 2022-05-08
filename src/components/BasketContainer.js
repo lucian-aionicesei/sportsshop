de8651221e4b700464e5c5e29bceaf423092a30e
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-05-08 22:01:47 +0200

    first commit

---
+++ src/components/BasketContainer.js
@@ -0,0 +1,12 @@
+import Basket from "./Basket"
+import CheckoutForm from "./CheckoutForm"
+
+export default function BasketContainer() {
+    return (
+        <section>
+            <Basket />
+            <CheckoutForm />
+        </section>
+    )
+    
+}
