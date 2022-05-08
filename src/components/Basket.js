27c3dd6e50dd88255407fb046b0eccd48a878226
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-05-08 23:35:24 +0200

    9-10 done

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,9 +1,23 @@
-export default function Basket(params) {
-    return (
-        <div>
-            <ul>
-                <li>Stuff</li>
-            </ul>
-        </div>
-    )
+export default function Basket(props) {
+
+  const initialValue = 0;
+  const sumWithInitial = props.basket.reduce(
+    (previousValue, currentObject) => previousValue + currentObject.price,
+    initialValue
+  );
+
+  return (
+    <div>
+      <ul>
+        {props.basket.map((item) => {
+          return (
+            <li>
+              {item.productdisplayname} ${item.price}
+            </li>
+          );
+        })}
+      </ul>
+      <p>Total: ${sumWithInitial}</p>
+    </div>
+  );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -3,13 +3,13 @@ export default function Product(props) {
     function buy() {
         props.setBasket(oldState => [...oldState, props.product])
     }
-    
+
   return (
     <article>
       <img src={`https://kea-alt-del.dk/t7/images/webp/640/${props.product.id}.webp`} alt="" />
       <h2>{props.product.productdisplayname}</h2>
       <p>{props.product.price}</p>
-      <button onClick={buy}>Buy</button>
+      <button onClick={buy}>Add</button>
     </article>
   );
 }
