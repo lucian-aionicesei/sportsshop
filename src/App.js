bb80cb1ef1ba4585fb5c2de8236d3b6becc00a35
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-05-08 22:40:16 +0200

    3.done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import {useState, useEffect} from "React";
 import Nav from "./components/Nav";
 import ProductList from "./components/ProductList";
 import BasketContainer from "./components/BasketContainer";
 import styles from "./App.module.css"
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect (() => {
+    async function getProducts() {
+      const res = await fetch ("https://kea-alt-del.dk/t7/api/products");
+      const data = await res.json();
+      setProducts(data);
+    }
+    getProducts();
+  }, []);
+
   return (
     <div className={styles.App}>
       <Nav />
